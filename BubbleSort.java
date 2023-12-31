b90d772167876972f87d283d62aac3e827983fec
aerlbaum3 <121773976+aerlbaum3@users.noreply.github.com>
2023-12-31 13:37:08 -0500

    Update BubbleSort.java
    Finished

--- BubbleSort.java
+++ BubbleSort.java
@@ -3,20 +3,14 @@ public class BubbleSort<T extends Comparable<T>> {
 	
 	private int comparisons = 0;//for the performance race
 	private int swaps = 0;
-	private T[] array;
 	
-//	public BubbleSort(T[] array) {
-//		this.array = array;
-//	}
-//	public BubbleSort() {
-//		
-//	}
 
 	public void performSort(T[] array) {
 		int n = array.length;
 		
-		for(int i = 0; i < n - i; i ++) {
-			for(int j = 0; j < n - i - 1; j++) {
+		
+		for(int i = 0; i < n - 1; i++) {
+			for(int j = 0; j < n - 1 - i; j++) {
 				comparisons++; // increment the comparison by one each time loop this array
 				//compare the indexes
 				if (array[j].compareTo(array[j + 1]) > 0) {   //swap needed			
@@ -34,7 +28,5 @@ public class BubbleSort<T extends Comparable<T>> {
 	public int getSwaps() {
 		return swaps;
 	}
-//	public T[] getArray() {
-//		return array;
-//	}
+
 }
