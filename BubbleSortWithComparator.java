6d05c14edb408a0916762fab302bfd03ce7cd4e4
aerlbaum3 <121773976+aerlbaum3@users.noreply.github.com>
2023-12-31 13:37:38 -0500

    Update BubbleSortWithComparator.java
    Finished

--- BubbleSortWithComparator.java
+++ BubbleSortWithComparator.java
@@ -3,22 +3,22 @@ import java.util.Comparator;
 public class BubbleSortWithComparator<T> {
 	
 	private T[] arr;
-	private Comparator<? super T> customComparator;
+	private Comparator<T> customComparator;
 	private int comparisons = 0;
-
-	public BubbleSortWithComparator(T[] arr, Comparator<? super T> customComparator) {
+	private int swaps = 0;
+	
+	public BubbleSortWithComparator(T[] arr) {
 		this.arr = arr;
-		this.customComparator = customComparator;
 	}
-	public void performSort() {
+	public void performSort(Comparator<T> customComparator) {
 		int n = arr.length;
 		
-		for(int i = 0; i < n - i; i ++) {
-			for(int j = 0; j < n - i - 1; j++) {
+		for(int i = 0; i < n - 1; i ++) {
+			for(int j = 0; j < n - 1 - i; j++) {
 				comparisons++; // increment the comparison by one each time loop this array
 				//compare the indexes
 				if(customComparator.compare(arr[j], arr[j+1]) > 0);
-					
+					swaps++;
 					T tempArray = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = tempArray;
@@ -26,10 +26,10 @@ public class BubbleSortWithComparator<T> {
 			}
 		}
 
-	public T[] getArray() {
-		return arr;
-	}
 	public int getComparisons() {
 		return comparisons;
+	}
+	public int getSwaps() {
+		return swaps;
 	}
 }
